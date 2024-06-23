cf7067e3b0d7494de8e75e54aad4cb6ce46297bf
Santi Ayechu <santi.ayechu@540deg.com>
2024-06-23 23:31:33 +0200

    [7] - Migrar codigo a Typescript y modificar formato/extension ficheros

---
+++ routes/usuarios/getUsuarios.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import { execute } from '../../services/getUsuariosService';
+import {getUsuariosSchema} from "./getUsuariosSchema";
+
+interface QueryString {
+    id?: string;
+    nombre?: string;
+    email?: string;
+}
+
+export default async function (fastify: FastifyInstance) {
+    fastify.get('/', getUsuariosSchema,
+        async function (request: FastifyRequest<{ Querystring: QueryString }>, reply: FastifyReply) {
+            const { id, nombre, email } = request.query;
+            const usuarios = await execute(fastify, id, nombre, email);
+            reply.code(200).send(usuarios);
+        }
+    );
+}
---
+++ services/getUsuariosService.ts
@@ -0,0 +1,31 @@
+import { FastifyInstance } from 'fastify';
+import '@fastify/sensible';
+
+interface Usuario {
+    id: string;
+    nombre: string;
+    email: string;
+}
+
+const execute = async (fastify: FastifyInstance, id?: string, nombre?: string, email?: string): Promise<Usuario[]> => {
+    const usuarios: Usuario[] = [
+        { id: "1", nombre: "juan", email: "juan@gmail.com" },
+        { id: "2", nombre: "juanlopez", email: "juanlopez@gmail.com" },
+        { id: "3", nombre: "juan", email: "juan@gmail.com" },
+    ];
+    if (!usuarios.length) {
+        throw fastify.httpErrors.notFound("No se encontraron usuarios.");
+    }
+    if (!id && !nombre && !email) {
+        return usuarios;
+    }
+
+    return usuarios.filter(
+        (usuario) =>
+            id === usuario.id ||
+            nombre === usuario.nombre ||
+            email === usuario.email
+    );
+};
+
+export { execute };
