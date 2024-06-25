f4d4ea77a51a815aea02fa4a8bfe0a31564e7c76
Santi Ayechu <santi.ayechu@540deg.com>
2024-06-25 09:37:16 +0200

    [2] - Añadir manejo de error en servicio del endpoint obtener usuarios.

--- services/getUsuariosService.ts
+++ services/getUsuariosService.ts
@@ -8,26 +8,30 @@ interface Usuario {
     email: string;
 }
 
-const execute = async (
+export const execute = async (
     fastify: FastifyInstance,
     id?: number,
     nombre?: string,
     email?: string
 ): Promise<Usuario[]> => {
-    const usuarios: Usuario[] = await prisma.user.findMany();
-    if (!usuarios.length) {
-        throw fastify.httpErrors.notFound("No se encontraron usuarios.");
-    }
-    if (!id && !nombre && !email) {
-        return usuarios;
-    }
+    try {
+        const usuarios: Usuario[] = await prisma.user.findMany();
+        if (!usuarios.length) {
+            throw fastify.httpErrors.notFound("No se encontraron usuarios.");
+        }
+        if (!id && !nombre && !email) {
+            return usuarios;
+        }
 
-    return usuarios.filter(
-        (usuario) =>
-            id == usuario.id ||
-            nombre === usuario.nombre ||
-            email === usuario.email
-    );
+        return usuarios.filter(
+            (usuario) =>
+                id == usuario.id ||
+                nombre === usuario.nombre ||
+                email === usuario.email
+        );
+    } catch (error) {
+        throw fastify.httpErrors.internalServerError(
+            "Error del servidor al intentar recuperar los usuarios."
+        );
+    }
 };
-
-export { execute };
