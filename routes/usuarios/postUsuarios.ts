00f6134c35e95b5dfd00d320aa7a3a62a4d00eb1
Santi Ayechu <santi.ayechu@540deg.com>
2024-06-25 11:13:22 +0200

    [11] - Extraer lógica de handler/controlador de endpoint store user

--- routes/usuarios/postUsuarios.ts
+++ routes/usuarios/postUsuarios.ts
@@ -1,23 +1,7 @@
-import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { postUsuariosSchema } from "../../services/postUsuariosSchema";
-import { execute } from "../../services/postUsuariosService";
-
-interface PostUsuarioBody {
-    nombre: string;
-    email: string;
-}
+import { FastifyInstance } from "fastify";
+import { storeUserSchema } from "../../application/controllers/storeUser/storeUserSchema";
+import { storeUserHandler } from "../../application/controllers/storeUser/storeUserController";
 
 export default async function (fastify: FastifyInstance) {
-    fastify.post(
-        "/",
-        postUsuariosSchema,
-        async function (
-            request: FastifyRequest<{ Body: PostUsuarioBody }>,
-            reply: FastifyReply
-        ) {
-            const { nombre, email } = request.body;
-            const usuarioCreado = await execute(fastify, nombre, email);
-            reply.code(201).send(usuarioCreado);
-        }
-    );
+    fastify.post("/", storeUserSchema, storeUserHandler);
 }
