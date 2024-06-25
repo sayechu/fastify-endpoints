9ae9291269235a3e3ef2317a406a3e721c33f7e8
Santi Ayechu <santi.ayechu@540deg.com>
2024-06-25 09:36:29 +0200

    [0] - Registrar plugin sensible para manejo de error

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import path from "node:path";
 import AutoLoad from "@fastify/autoload";
+import sensible from "@fastify/sensible";
 
 export default async function (fastify: FastifyInstance, opts: any) {
+    fastify.register(sensible);
     fastify.register(AutoLoad, {
         dir: path.join(__dirname, "routes"),
         options: Object.assign({}, opts),
