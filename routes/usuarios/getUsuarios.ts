97bcf6b47aa9d960938f7ccee7fb1d27643c469d
Santi Ayechu <santi.ayechu@540deg.com>
2024-06-25 13:01:57 +0200

    [11] - Eliminar rutas getUsuarios y postUsuarios para unificarlas en un propio routes

--- routes/usuarios/getUsuarios.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { getUsersSchema } from "../../application/controllers/getUsers/getUsersSchema";
-import { getUsersHandler } from "../../application/controllers/getUsers/getUsersController";
-
-export default async function (fastify: FastifyInstance) {
-    fastify.get("/", getUsersSchema, getUsersHandler);
-}
--- routes/usuarios/postUsuarios.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { storeUserSchema } from "../../application/controllers/storeUser/storeUserSchema";
-import { storeUserHandler } from "../../application/controllers/storeUser/storeUserController";
-
-export default async function (fastify: FastifyInstance) {
-    fastify.post("/", storeUserSchema, storeUserHandler);
-}
