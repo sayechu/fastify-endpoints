f8f7a02bb04f0a9ebbbafd30b4d138103f2fb551
Santi Ayechu <santi.ayechu@540deg.com>
2024-06-25 08:29:53 +0200

    [2] - Cambiar lógica de endpoint crear usuario para crear tupla con datos de usuario en la DB

--- services/postUsuariosService.ts
+++ services/postUsuariosService.ts
@@ -1,17 +1,20 @@
 import { FastifyInstance } from "fastify";
+import prisma from "../utils/prisma";
 
 interface Usuario {
-    id: string;
+    id: number;
     nombre: string;
     email: string;
 }
 
-const execute = async (
-    fastify: FastifyInstance,
-    nombre: string,
-    email: string
-): Promise<Usuario> => {
-    return { id: "20", nombre: nombre, email: email };
+const execute = async (nombre: string, email: string): Promise<Usuario> => {
+    const newUser: Usuario = await prisma.user.create({
+        data: {
+            nombre: nombre,
+            email: email,
+        },
+    });
+    return newUser;
 };
 
 export { execute };
