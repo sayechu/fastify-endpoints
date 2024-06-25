c38d13b6621cdbb944e676e9be46cb3e8efcac7b
Santi Ayechu <santi.ayechu@540deg.com>
2024-06-25 08:26:23 +0200

    [11] - Refactorizar código eliminando importación plugins

--- app.ts
+++ app.ts
@@ -2,18 +2,9 @@ import { FastifyInstance } from "fastify";
 import path from "node:path";
 import AutoLoad from "@fastify/autoload";
 
-const options = {};
-
-module.exports = async function (fastify: FastifyInstance, opts: any) {
-    fastify.register(AutoLoad, {
-        dir: path.join(__dirname, "plugins"),
-        options: Object.assign({}, opts),
-    });
-
+export default async function (fastify: FastifyInstance, opts: any) {
     fastify.register(AutoLoad, {
         dir: path.join(__dirname, "routes"),
         options: Object.assign({}, opts),
     });
-};
-
-module.exports.options = options;
+}
